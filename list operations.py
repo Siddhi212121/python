ec5f67d81352d9bf3defb3e5d92a0400250785a6
Siddhi212121 <105291889+Siddhi212121@users.noreply.github.com>
2023-07-15 20:35:19 +0530

    Create list operations.py

---
+++ list operations.py
@@ -0,0 +1,10 @@
+E={0,2,4,6,8};
+
+N={1,2,3,4,5};
+
+print("Union of E and N is:",E|N)
+print("intersection of E and N is:",E&N)
+print("Union of E and N is:",E|N)
+print("difference between E and N is:",E-N)
+print("symmetric difference between E and N is:",E^N)
+
